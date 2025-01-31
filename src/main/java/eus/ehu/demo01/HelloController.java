c54c75f1b3aba9a8ceac09694a98bd8d4c5d165a
YosephUnzueta <yunzueta002@ikasle.ehu.eus>
2025-01-31 11:22:04 +0100

    ; added

--- src/main/java/eus/ehu/demo01/HelloController.java
+++ src/main/java/eus/ehu/demo01/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
         System.out.println("Alex was here.");
-        System.out.println("Yoseph is better.")
+        System.out.println("Yoseph is better.");
     }
 }
